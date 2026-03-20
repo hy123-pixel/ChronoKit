5689857ebf94707e225d6c9a2598abd8041b8cb0
hy <wish.hy1996@gmail.com>
2026-03-20 17:54:31 +0800

    Unify ExampleApp chrome and launch screen

--- Demo/ExampleApp/ExampleApp.swift
+++ Demo/ExampleApp/ExampleApp.swift
@@ -2,9 +2,25 @@ import SwiftUI
 
 @main
 struct ExampleApp: App {
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
+        appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterialDark)
+        appearance.backgroundColor = UIColor(red: 0.05, green: 0.13, blue: 0.14, alpha: 1.0)
+        appearance.shadowColor = .clear
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().tintColor = .white
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.dark)
         }
     }
 }
